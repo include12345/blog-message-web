897422d68468cf604543094a15d9d4aecebef0d4
李贺宾 <lihebin@wosai-inc.com>
2018-12-02 22:40:26 +0800

    中场休息

---
+++ build/webpack.base.conf.js
@@ -0,0 +1,8 @@
+const path = require('path')
+const utils = require('./utils')
+const config = require('../config')
+const vueLoaderConfig = require('./vue-loader.conf')
+
+function resolve(dir) {
+    return path.join(__dirname, '..', dir)
+}
